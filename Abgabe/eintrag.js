49dd7771bcfea9608a14abdb08d061b88ab4e004
LenaNscott <lena.nerz@web.de>
2021-07-11 14:34:14 +0200

    video wird gelöscht wenn es fertig ist, css änderung tablet video

--- Abgabe/eintrag.js
+++ Abgabe/eintrag.js
@@ -1,19 +1,7 @@
 "use strict";
-videoAbspielen();
-async function videoAbspielen() {
-    let video = document.createElement("video");
-    video.id = "video";
-    video.src = "Medien/NeverGonnaGiveYouUpVideo.mp4";
-    video.autoplay = true;
-    video.load();
-    document.getElementById("body").appendChild(video);
-    await SleepVideo(9000);
-    document.getElementById("body").removeChild(video);
-}
-let highscoreEintrag = document.getElementById("fertig");
-highscoreEintrag.addEventListener("click", datenbankSchreiben);
 let anzeigeZeit = document.getElementById("timerAnzeige");
 let anzeigeScore = document.getElementById("punkteAnzeige");
+let highscoreEintrag = document.getElementById("fertig");
 let cookiesSplit = [];
 //console.log(document.cookie);
 cookiesSplit = document.cookie.split("; ");
@@ -21,8 +9,18 @@ cookiesSplit = document.cookie.split("; ");
 let klicks = parseInt(cookiesSplit[0].split("=")[1]);
 let sek = parseInt(cookiesSplit[1].split("=")[1]);
 let punkte = Math.round(80000 / (klicks + (sek / 3)));
-anzeigeScore.innerHTML = punkte.toString();
-anzeigeZeit.innerHTML = secString(sek.toString());
+let video = document.createElement("video");
+video.id = "video";
+video.src = "Medien/NeverGonnaGiveYouUpVideo.mp4";
+video.autoplay = true;
+video.load();
+document.getElementById("body").appendChild(video);
+video.onended = function () {
+    document.getElementById("body").removeChild(video);
+    anzeigeScore.innerHTML = punkte.toString();
+    anzeigeZeit.innerHTML = secString(sek.toString());
+    highscoreEintrag.addEventListener("click", datenbankSchreiben);
+};
 async function datenbankSchreiben() {
     let formData = new FormData(document.forms[0]);
     let query = new URLSearchParams(formData);
@@ -31,8 +29,5 @@ async function datenbankSchreiben() {
     //console.log(datenbankEintrag);
     await eintragDatenbank(datenbankEintrag);
     weiterleitungSeite(highscoreEintrag.id);
-}
-function SleepVideo(milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 //# sourceMappingURL=eintrag.js.map
--- Abgabe/eintrag.ts
+++ Abgabe/eintrag.ts
@@ -1,22 +1,6 @@
-videoAbspielen();
-
-async function videoAbspielen(): Promise<void> {
-let video: HTMLVideoElement = document.createElement("video");
-video.id = "video";
-video.src = "Medien/NeverGonnaGiveYouUpVideo.mp4";
-video.autoplay = true;
-video.load();
-document.getElementById("body").appendChild(video);
-await SleepVideo(9000);
-document.getElementById("body").removeChild(video);
-}
-
-let highscoreEintrag: HTMLElement = document.getElementById("fertig");
-highscoreEintrag.addEventListener("click", datenbankSchreiben);
-
-
 let anzeigeZeit: HTMLElement = document.getElementById("timerAnzeige");
 let anzeigeScore: HTMLElement = document.getElementById("punkteAnzeige");
+let highscoreEintrag: HTMLElement = document.getElementById("fertig");
 
 let cookiesSplit: string [] = [];
 //console.log(document.cookie);
@@ -26,9 +10,18 @@ let klicks: number = parseInt(cookiesSplit[0].split("=")[1]);
 let sek: number = parseInt(cookiesSplit[1].split("=")[1]);
 let punkte: number = Math.round(80000 / (klicks + (sek / 3)));
 
-anzeigeScore.innerHTML = punkte.toString();
-anzeigeZeit.innerHTML = secString(sek.toString());
-
+let video: HTMLVideoElement = document.createElement("video");
+video.id = "video";
+video.src = "Medien/NeverGonnaGiveYouUpVideo.mp4";
+video.autoplay = true;
+video.load();
+document.getElementById("body").appendChild(video);
+video.onended = function() {
+    document.getElementById("body").removeChild(video);
+    anzeigeScore.innerHTML = punkte.toString();
+    anzeigeZeit.innerHTML = secString(sek.toString());
+    highscoreEintrag.addEventListener("click", datenbankSchreiben);
+};
 
 async function datenbankSchreiben(): Promise <void> {
     let formData: FormData = new FormData(document.forms[0]);
@@ -40,8 +33,3 @@ async function datenbankSchreiben(): Promise <void> {
     await eintragDatenbank(datenbankEintrag); 
     weiterleitungSeite(highscoreEintrag.id);
 }
-
-function SleepVideo(milliseconds: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
-   }
-
